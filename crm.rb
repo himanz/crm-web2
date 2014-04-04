db786e766ad38c741392911543e4abe2c4d9f728
Jonathan Chang <jonoschang7@gmail.com>
2014-04-04 16:34:48 -0400

    Fix create new contact

--- crm.rb
+++ crm.rb
@@ -32,7 +32,7 @@ get '/contacts/new' do
 	erb :new_contact
 end
 
-put '/contacts' do
+post '/contacts' do
 	Contact.create(
 		:first_name => params[:first_name],
 		:last_name => params[:last_name],
